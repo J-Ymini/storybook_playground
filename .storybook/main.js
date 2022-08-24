d11e4db7b9b740ef9791e4b4cc1151ba2a88c42b
yongmin.jeon <dydalsdl1414@gmail.com>
2022-08-24 18:04:48 +0900

    feat: config addons

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,17 +3,17 @@ module.exports = {
   // 변경시 storybook 재시작 필요
 
   // stories: 스토리파일 위치 경로
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
 
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    '@storybook/preset-create-react-app',
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/preset-create-react-app",
   ],
   // framework specific configurations to help the loading and building process.
-  framework: '@storybook/react',
+  framework: "@storybook/react",
   core: {
-    builder: '@storybook/builder-webpack5',
+    builder: "@storybook/builder-webpack5",
   },
 };
--- src/components/ChakraButton/ChakraButton.stories.tsx
+++ src/components/ChakraButton/ChakraButton.stories.tsx
@@ -1,12 +1,36 @@
 import { Button } from "@chakra-ui/react";
 import STORIES_NAME from "../STORIES_NAME";
 
-import { ComponentMeta } from "@storybook/react";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
 
 export default {
   title: `${STORIES_NAME.FORM}/ChakraButton`,
   component: Button,
+  args: {
+    children: "default",
+  },
+  argTypes: {
+    colorScheme: {
+      options: ["red", "blue", "yellow", "purple"],
+      control: { type: "radio" },
+    },
+    children: {
+      control: { type: "text" },
+    },
+    onClick: {
+      action: "clicked",
+    },
+  },
 } as ComponentMeta<typeof Button>;
 
 export const Success = () => <Button colorScheme="green">Success</Button>;
 export const Danger = () => <Button colorScheme="red">Danger</Button>;
+
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+export const Warning = Template.bind({});
+Warning.args = {
+  colorScheme: "yellow",
+  textColor: "white",
+  children: "Button",
+};
